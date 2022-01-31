b5cdcd9cad3a77d843621f02c3e2c79f5254d1b1
duffield22 <duffield22@marshall.edu>
2022-01-30 22:42:39 -0500

    nav fix

--- components/navbar.inc.php
+++ components/navbar.inc.php
@@ -73,6 +73,12 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){?>
 
               <li class="d-flex ms-auto">
                 <ul>
+                  <li class="nav-item">
+                      <a class="nav-link" href="contact.php"  role="button"  aria-expanded="false">CONTACT</a></li>
+                  </li>
+                </ul>
+
+                <ul>
                   <!-- Dropdown menu Account -->
                   <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
