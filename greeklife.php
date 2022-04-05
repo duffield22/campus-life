ac3d2179d1468d4ee73e6152b9931aaaa0341dbe
duffield22 <duffield22@marshall.edu>
2022-04-05 17:54:10 -0400

    greek

---
+++ greeklife.php
@@ -0,0 +1,57 @@
+<?php
+// Initialize the session
+session_start();
+?>
+<?php require('components/head.inc.php'); ?>
+
+<?php include('components/navbar.inc.php'); ?>
+<!-- BODY SECTION -->
+
+  <!-- About Start -->
+  <section class="section " id="about">
+      <div class="container">
+          <div class="row justify-content-center">
+              <div class="col-lg-6 col-md-8">
+                  <div class="text-center mb-5">
+                      <h2 class="highlight header-colorize text-uppercase mb-3 mt-3">Greek Life</h2>
+                      <p class="text-sub size-16">Join a Marshall Fraternity or Sorority!</p>
+                  </div>
+              </div>
+          </div>
+          <div class="row align-items-center">
+              <div class="col-lg-5">
+                  <img src="http://www.marshall.edu/student-activities/files/2014/01/OFSL-Logo-300x249.jpg" alt="" class="img-fluid mx-auto d-block">
+              </div>
+              <div class="col-lg-6 offset-lg-1">
+                  <div class="text-sub mt-5 mt-lg-0">
+                      <h3 class="title font-weight-medium mb-4 header-colorize">Our Mission</h3>
+                        <p class="mb-4 size-18">The Office of Fraternity and Sorority Life commits to the holistic development and success of all students, the cultivation of inclusive spaces and communities, and the fostering of meaningful relationships and lifelong learning.</p>
+                      <p class="mb-4 size-18">Marshall University has been the host of fraternities and sororities for 97 years. Today, the community has over 500 students holding membership in 17 organizations. The Office of Fraternity and Sorority Life (FSL) focuses on scholarship, unity, service and philanthropy, and leadership. Several of Marshall’s most distinguished graduates are members of fraternities and sororities.</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+  <!-- About End -->
+
+
+  <!-- About Start -->
+  <section class="section bg-grey" id="about">
+      <div class="container">
+  <h2 class="highlight header-colorize text-uppercase mb-5 mt-3">Fraternities</h2>
+          <div class="row align-items-center">
+
+              <div class="col-lg-6 offset-lg-1">
+                  <div class="text-sub mt-5 mt-lg-0">
+                      <h3 class="title font-weight-medium mb-4 header-colorize">Our Mission</h3>
+                        <p class="mb-4 size-18">The Office of Fraternity and Sorority Life commits to the holistic development and success of all students, the cultivation of inclusive spaces and communities, and the fostering of meaningful relationships and lifelong learning.</p>
+                      <p class="mb-4 size-18">Marshall University has been the host of fraternities and sororities for 97 years. Today, the community has over 500 students holding membership in 17 organizations. The Office of Fraternity and Sorority Life (FSL) focuses on scholarship, unity, service and philanthropy, and leadership. Several of Marshall’s most distinguished graduates are members of fraternities and sororities.</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+  <!-- About End -->
+
+
+<?php require('components/footer.inc.php'); ?>
